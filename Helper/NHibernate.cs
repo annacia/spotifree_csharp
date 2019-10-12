e1ed5e36c9d9cb52e90223d0043fdea971a936ca
Carol Aureliano <carol.inhasz@hotmail.com>
2019-10-11 22:10:38 -0300

    untracked files on nhibernate_test: 434472d Estruturando as classes

---
+++ Helper/NHibernate.cs
@@ -0,0 +1,20 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Spotifree.Helper
+{
+    public class NHibernate_Helper
+    {
+        public static Configuration ConfigurationRecover()
+        {
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+            return cfg;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate.Cfg;
+using System;
+using Spotifree.Helper;
+using NHibernate;
+
+namespace Spotifree
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Configuration cfg = NHibernate_Helper.ConfigurationRecover();
+            ISessionFactory sessionFactory = cfg.BuildSessionFactory();
+            ISession session = sessionFactory.OpenSession();
+
+            Category cate = new Category();
+            cate.Name = "jrock";
+            cate.Created = new DateTime();
+
+            ITransaction transacao = session.BeginTransaction();
+            session.Save(cate);
+            transacao.Commit();
+            session.Close();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
