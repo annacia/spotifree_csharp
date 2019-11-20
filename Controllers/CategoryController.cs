cbf52104c9e101a8cdf55c77f0496bd70ae84920
Thierry Alves <thierryalves.oliveira@hotmail.com>
2019-11-20 16:14:30 -0300

    CRUD category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,86 @@
+﻿using Spotifree.DAO;
+using Spotifree.Mapper;
+using Spotifree.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Spotifree.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        // GET: api/User
+        //public IEnumerable<string> GetAll()
+        //{
+
+        //}
+
+        // GET: api/User/5
+        public IHttpActionResult Get(int id)
+        {
+            DAO_Category select = new DAO_Category();
+            Category retorno = (Category)select.SearchById(id);
+
+            return ResponseMessage(Request.CreateResponse<Object>(HttpStatusCode.OK, retorno));
+        }
+
+        // POST: api/User
+        public IHttpActionResult Post([FromBody]Category value)
+        {
+            try
+            {
+                Mapper_Category mapper = new Mapper_Category();
+                mapper.validate(value);
+                mapper.Model = value;
+                mapper.Register();
+
+                return ResponseMessage(Request.CreateResponse<Object>(HttpStatusCode.OK, mapper.Model));
+            }
+            catch (Exception e)
+            {
+                var retorno = new
+                {
+                    Erro = e.Message
+                };
+
+                return ResponseMessage(Request.CreateResponse<Object>(HttpStatusCode.OK, retorno));
+            }
+        }
+
+        // PUT: api/User/5
+        public IHttpActionResult Put(int id, [FromBody]Category value)
+        {
+            try
+            {
+                value.Id = id;
+                Mapper_Category update = new Mapper_Category();
+                update.validate(value);
+                update.Model = value;
+                update.Update();
+
+                return ResponseMessage(Request.CreateResponse<Object>(HttpStatusCode.OK, update.Model));
+            }
+            catch (Exception e)
+            {
+                var retorno = new
+                {
+                    Erro = e.Message
+                };
+
+                return ResponseMessage(Request.CreateResponse<Object>(HttpStatusCode.OK, retorno));
+            }
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+            Mapper_Category delete = new Mapper_Category();
+            delete.Model = delete.Load(id);
+            delete.Delete();
+        }
+    }
+}
+
